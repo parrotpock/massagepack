d787f86163104736a3c2ec50305107d8cb4c4c7f
Mark Frawley <markfrawley@gmail.com>
2019-06-15 12:52:05 +0200

    wrote test for pack and unpack Int32

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,13 +1,14 @@
 import Test.Tasty (defaultMain, testGroup)
 import Test.Tasty.HUnit (assertEqual, testCase)
 import MsgPack (pack, unpack)
+import Data.Int (Int32)
 
 main = defaultMain unitTests
 
 unitTests =
   testGroup
     "Unit tests"
-      [testReverseBoolTrue, testReverseBoolFalse, testReverseNil]
+      [testReverseBoolTrue, testReverseBoolFalse, testReverseNil, testReverseInt32]
 
 testReverseBoolTrue =
   testCase "Deserialising a serialised True (bool) gives a Just True" $ assertEqual [] (unpack $ pack True) (Just True)
@@ -17,3 +18,7 @@ testReverseBoolFalse =
 
 testReverseNil =
   testCase "Deserialising a serialised Nil gives a Just Nil" $ assertEqual [] (unpack $ pack ()) (Just ())
+
+testReverseInt32 =
+  let val = 234234::Int32 in
+  testCase "Deserialising a serialised Int32 gives a Just Int32" $ assertEqual [] (unpack $ pack val) (Just val)
